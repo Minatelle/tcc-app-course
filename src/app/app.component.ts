2dd0fb6cd4b53b6e75fcf4ab02550135e774f6bd
Minatelle <gminatelle@gmail.com>
2023-08-16 18:28:35 -0300

    [WI-5] Refactor Calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig, Translation } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+import { CalendarTranslation } from './shared/calendar-translation';
 
 @Component({
   selector: 'app-root',
@@ -9,53 +10,10 @@ import { PrimeNGConfig, Translation } from 'primeng/api';
 export class AppComponent implements OnInit {
   public title = 'tcc-app-course';
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(private primeConfig: PrimeNGConfig) {}
 
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-    this.primengConfig.setTranslation({
-      dayNames: [
-        'Domingo',
-        'Segunda',
-        'Terça',
-        'Quarta',
-        'Quinta',
-        'Sexta',
-        'Sábado',
-      ],
-      dayNamesShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'],
-      dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
-      firstDayOfWeek: 0,
-      monthNames: [
-        'Janeiro',
-        'Fevereiro',
-        'Março',
-        'Abril',
-        'Maio',
-        'Junho',
-        'Julho',
-        'Agosto',
-        'Setembro',
-        'Outubro',
-        'Novembro',
-        'Dezembro',
-      ],
-      monthNamesShort: [
-        'Jan',
-        'Fev',
-        'Mar',
-        'Abr',
-        'Mai',
-        'Jun',
-        'Jul',
-        'Ago',
-        'Set',
-        'Out',
-        'Nov',
-        'Dez',
-      ],
-      today: 'Hoje',
-      clear: 'Limpar',
-    } as Translation);
+  public ngOnInit() {
+    this.primeConfig.ripple = true;
+    this.primeConfig.setTranslation(CalendarTranslation);
   }
 }
