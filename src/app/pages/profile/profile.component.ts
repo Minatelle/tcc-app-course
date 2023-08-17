ce44147a2badf2a4af36e169464f4bca4646ca4b
Minatelle <gminatelle@gmail.com>
2023-08-17 17:44:45 -0300

    [WI-3] Save profile data on cache + workspace config

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -13,16 +13,30 @@ export class ProfileComponent {
   public dateToday: string = new Date().toLocaleDateString();
   public formGroup: FormGroup = new FormGroup({
     name: new FormControl(''),
-    birthDate: new FormControl(''),
+    birthDate: new FormControl<Date | null>(null),
   });
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private cookieService: CookieService
+  ) { }
 
   public navigateToHome() {
     this.router.navigate(['/']);
   }
 
   public onBasicUploadAuto(event: UploadEvent) {
-    console.log('Send!');
+    console.log('Send!', event);
+  }
+
+  public onSubmit(): void {
+    this.setCookie('name', this.formGroup.value.name);
+    this.setCookie('birthDate', this.formGroup.value.birthDate);
+  }
+
+  private setCookie(key: string, value: string | null): void {
+    if (value) {
+      this.cookieService.set(key, value);
+    }
   }
 }
